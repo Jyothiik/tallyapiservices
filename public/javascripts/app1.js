cfc3e34c3997c32f33fabd4a02286cda15eb795b
jyothi <jyo.nsk17@gmail.com>
2017-03-01 16:21:24 +0530

    modify general

--- public/javascripts/app1.js
+++ public/javascripts/app1.js
@@ -4,13 +4,39 @@ alert('our form is amazing');
 validationApp.controller('mainController', function($scope) {
  //$scope.serverip = ''
   // function to submit the form after all validation has occurred
+
+
+
   $scope.submitForm = function(isValid) {
  alert('our form is amazing');
     // check to make sure the form is completely valid
     if (isValid) {
       alert('our form is amazing');
+
     }
 
   };
 
 });
+
+//  <script>
+//    angular.module("myapp", [])
+//      .controller("MyController", function($scope) {
+//        $scope.register = {};
+//        $scope.register.countryId = "1";
+//
+//        $scope.register.countries = [{
+//          id: "1",
+//          name: "India"
+//        }, {
+//          id: "2",
+//          name: "USA"
+//        }, {
+//          id: "3",
+//          name: "UK"
+//        }, {
+//          id: "4",
+//          name: "Nepal"
+//        }];
+//      });
+//  </script>
